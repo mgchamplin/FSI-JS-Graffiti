d3f93e7954562f4bc47443233d8d2ac08dfb4c63
Michael Champlin <mchamplin@sbcglobal.net>
2021-11-29 19:15:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Select some elements...
+let header = document.querySelector('#page-header')
+//header.style.textAlign = "left"
+
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = "scaleX(-1)";
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+    dogNames[i].style.fontSize = '70px'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = "red"
